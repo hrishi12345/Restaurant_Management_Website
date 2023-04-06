398775184b1f3574d0f6a4eba735e1f73a1a4ebc
hrishi12345 <hrajgure6@gmail.com>
2023-04-06 15:48:16 +0530

    Add button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import Header from "./components/Layout/Header";
+import { Fragment } from 'react';
 
-import Meals from "./components/Meals/Meals";
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
 
 function App() {
   return (
-    <div>
+    <Fragment>
       <Header />
-      <Meals />
-    </div>
+      <main>
+        <Meals />
+      </main>
+    </Fragment>
   );
 }
 
